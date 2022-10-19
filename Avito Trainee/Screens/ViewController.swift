60bc2b742960d61a67df6fe9b7f78f38e6957176
Pavel Kachan <KachanPG@yandex.ru>
2022-10-19 19:52:35 +0300

    Данные из интернета успешно получены, NetworkManager работает корректно

--- Avito Trainee/Screens/ViewController.swift
+++ Avito Trainee/Screens/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var welcomeForView: Welcome?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        NetworkManager.shared.fetchData(from: Link.testApi.rawValue) { result in
+            switch result {
+            case .success(let welcome):
+                self.welcomeForView = welcome
+                print("Success")
+                print(welcome)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
 
