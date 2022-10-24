154cd842929d74f94d6d8bdc47cdf51c80fd324a
Pavel Kachan <KachanPG@yandex.ru>
2022-10-24 12:10:38 +0300

    Добавил таймер, который через час после кэширования очищает кэш

--- Avito Trainee/Services/NetworkManager.swift
+++ Avito Trainee/Services/NetworkManager.swift
@@ -17,6 +17,7 @@ final class NetworkManager {
     
     static let shared = NetworkManager()
     
+    // Values for caching
     let cacheKey: NSString = "key"
     var welcomeCache = NSCache<NSString, WelcomeHolder>()
     
@@ -45,9 +46,17 @@ final class NetworkManager {
                     let welcome = try JSONDecoder().decode(Welcome.self, from: data)
                     DispatchQueue.main.async {
                         complition(.success(welcome))
+                        
+                        // Caching
                         let welcomeClass = WelcomeHolder(welcomeHolder: welcome)
                         self.welcomeCache.setObject(welcomeClass, forKey: self.cacheKey)
                         print("Cache the struct")
+                        
+                        // Timer for hour then clear cache
+                        _ = Timer.scheduledTimer(withTimeInterval: 3600.0, repeats: true) { timer in
+                            self.welcomeCache.removeObject(forKey: self.cacheKey)
+                            print("Timer!")
+                        }
                     }
                 } catch {
                     complition(.failure(.decodingError))
